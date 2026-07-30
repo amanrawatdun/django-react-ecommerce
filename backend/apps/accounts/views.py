134cce5ec7d5968b2adab454a20fb42f23042a10
Aman Rawat <amanwtsre@gmail.com>
2026-07-31 00:43:42 +0530

    feat: initail commit with backend folder structure and settings setup

---
+++ backend/apps/accounts/views.py
@@ -0,0 +1,10 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+# Create your views here.
+
+@api_view(['GET'])
+def hello(request):
+    return Response({
+        "message":"hello django rest framework"
+    })
